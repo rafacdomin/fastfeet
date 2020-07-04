60ddf6823e7bb5b60cb1d7a18c50bd162b37053c
rafacdomin <rafacdomin@gmail.com>
2020-07-04 20:38:27 -0300

    Orders table and routes

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,144 @@
+import * as Yup from 'yup';
+import Order from '../models/Order';
+import File from '../models/File';
+import Recipient from '../models/Recipient';
+import Deliveryman from '../models/Deliveryman';
+
+class OrderController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      signature_id: Yup.number().notRequired(),
+      product: Yup.string().required(),
+      canceled_at: Yup.date().notRequired(),
+      start_date: Yup.date().notRequired(),
+      end_date: Yup.date().notRequired(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erorr: 'Validation fails' });
+    }
+
+    const { id, recipient_id, deliveryman_id, product } = await Order.create(
+      req.body
+    );
+
+    return res.json({ id, recipient_id, deliveryman_id, product });
+  }
+
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const orders = await Order.findAll({
+      attributes: [
+        'id',
+        'product',
+        'deliveryman_id',
+        'recipient_id',
+        'signature_id',
+        'canceled_at',
+        'start_date',
+        'end_date',
+      ],
+      include: [
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['url', 'name', 'path'],
+        },
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['name', 'cep', 'cidade', 'rua', 'numero', 'complemento'],
+        },
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['name', 'email', 'avatar_id'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['url', 'name', 'path'],
+            },
+          ],
+        },
+      ],
+      limit: 20,
+      offset: (page - 1) * 20,
+      order: ['created_at'],
+    });
+
+    return res.json(orders);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      signature_id: Yup.number(),
+      product: Yup.string(),
+      canceled_at: Yup.date(),
+      start_date: Yup.date(),
+      end_date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: 'id not provided' });
+    }
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res.status(400).json({ error: 'Order not found' });
+    }
+
+    const {
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      product,
+      canceled_at,
+      start_date,
+      end_date,
+    } = await order.update(req.body);
+
+    return res.json({
+      id,
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      product,
+      canceled_at,
+      start_date,
+      end_date,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: 'id not provided' });
+    }
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res.status(400).json({ error: 'Order not found' });
+    }
+
+    await order.destroy();
+
+    return res.json({ message: 'Order has been removed' });
+  }
+}
+
+export default new OrderController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import OrderController from './app/controllers/OrderController';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -21,6 +22,11 @@ routes.post('/deliverymans', DeliverymanController.store);
 routes.get('/deliverymans', DeliverymanController.index);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
+
+routes.post('/orders', OrderController.store);
+routes.get('/orders', OrderController.index);
+routes.put('/orders/:id', OrderController.update);
+routes.delete('/orders/:id', OrderController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
